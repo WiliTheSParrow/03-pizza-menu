1bb2a3804a5aee330f4c6cfb77b803e7a8cefe2b
WiliTheSparrow <w.julia91@gmail.com>
2024-02-06 11:41:33 +0100

    JS logic in components

--- src/index.js
+++ src/index.js
@@ -72,6 +72,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour < +closeHour) alert("We're currenlty open!");
+  // else alert("Sorry we're closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currenlty open</footer>
   );
